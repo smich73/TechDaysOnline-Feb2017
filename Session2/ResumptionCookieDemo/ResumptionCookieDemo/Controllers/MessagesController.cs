4f7b8a468e7584e57216fb848632a20f39838e8e
James Mann <james.mann@bcs.org>
2017-01-29 13:45:29 +0000

    added resumption cookie proactive demo

---
+++ Session2/ResumptionCookieDemo/ResumptionCookieDemo/Controllers/MessagesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ResumptionCookieDemo
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        // Credit: Robin Osborne
+        // http://robinosborne.co.uk/2017/01/02/sending-proactive-botframework-messages
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            var resumptionCookie = new ResumptionCookie(activity);
+            var data = JsonConvert.SerializeObject(resumptionCookie);
+            
+            // For demonstration - save the cookie to disk.  For a real application
+            // save to your persistent store - e.g. blob storage, table storage, document db, etc
+            File.WriteAllText(System.Web.Hosting.HostingEnvironment.MapPath("~/cookie.json"), data);
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }        
+    }
+}
