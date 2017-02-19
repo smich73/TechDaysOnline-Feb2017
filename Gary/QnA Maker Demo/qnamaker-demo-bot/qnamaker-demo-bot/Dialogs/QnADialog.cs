875dd4dd6c05a06e4691d462ead5991bd5142fd9
DESKTOP-K8O6TUG\gary <gary@garypretty.co.uk>
2017-02-19 13:54:52 +0000

    Added no match handler

--- Gary/QnA Maker Demo/qnamaker-demo-bot/qnamaker-demo-bot/Dialogs/QnADialog.cs
+++ Gary/QnA Maker Demo/qnamaker-demo-bot/qnamaker-demo-bot/Dialogs/QnADialog.cs
@@ -1,5 +1,7 @@
 ﻿using QnAMakerDialog;
 using System;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading.Tasks;
 
 namespace QnaMakerDemo.Dialogs
 {
@@ -7,5 +9,10 @@ namespace QnaMakerDemo.Dialogs
     [QnAMakerService("8f833e867b25443b95d8c23cd367f7ce", "f7cf425a-2801-4854-bb24-75a4b33f9d2d")]
     public class QnADialog : QnAMakerDialog<object>
     {
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            await context.PostAsync("Sorry, I could't find an answer to your question.");
+            context.Wait(MessageReceived);
+        }
     }
 }
